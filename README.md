0f1cbd74a8bb4e8312083d3108c84e0a740749fe
shreyalal <shreya15lal@gmail.com>
2021-10-02 00:40:18 +0530

    second commit

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ This is a Banana translator app. The app asks user to input text and then transl
 # How to use the app
 
 1. Enter yout text in input field.
-2. Click on the translate button to get the trnslation
+2. Click on the translate button to get the translation
 
--- index.js
+++ index.js
@@ -4,17 +4,20 @@ var output = document.querySelector("#output-area");
 
 var url= "https://api.funtranslations.com/translate/minion.json"
 
-function errHandler(error){
+function errHandler(error)
+{
     console.log("error occured" + error);
     alert("Something is wrong with the server");
 }
 
-function generateUrl(text){
+function generateUrl(text)
+{
     return url + "?" + "text=" + text;
 }
 
 
-function clickEventHandler() {
+function clickEventHandler() 
+{
     
     var inputText = input.value;
     fetch(generateUrl(inputText)).then(response => response.json()).then(json => {
